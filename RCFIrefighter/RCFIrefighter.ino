f6a10b11853a4640fa9129f17db8f15aaccbd628
Odysseas95 <o.dy.se@hotmail.com>
2017-12-27 04:53:13 +0200

    FLame and Bluetooth

--- RCFIrefighter/RCFIrefighter.ino
+++ RCFIrefighter/RCFIrefighter.ino
@@ -1,22 +1,47 @@
-//*** 1- Documentation
-//This program is used to control a robot car using a app that communicates with Arduino trough a bluetooth module
-
 #include <AFMotor.h>
 
+const int FsensorMin = 0;     // sensor minimum
+const int FsensorMax = 1024;  // sensor maximum
 //creates two objects to control the terminal 3 and 4 of motor shield 
-AF_DCMotor motor1(3); 
-AF_DCMotor motor2(4); 
+AF_DCMotor motor1(1); 
+AF_DCMotor motor2(2); 
 
 char command; 
 
 void setup() 
 {       
-  Serial.begin(9600);  //Set the baud rate to your Bluetooth module.
+  pinMode(51, OUTPUT);
+  Serial1.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop(){
-  if(Serial.available() > 0){ 
-    command = Serial.read(); 
+  
+//FLAME SENSOR
+// read the sensor on analog A14:
+  int FsensorReading = analogRead(A14);
+  int range = map(FsensorReading, FsensorMin, FsensorMax, 0, 3);// map the sensor range (four options)
+  
+  // range value:
+  switch (range) {
+  case 0:    // A fire closer than 1.5 feet away.
+    Serial.println("** Close Fire **");
+    digitalWrite(51, HIGH);
+    break;
+  case 1:    // A fire between 1-3 feet away.
+    Serial.println("** Distant Fire **");
+    digitalWrite(51, HIGH);
+    break;
+  case 2:    // No fire detected.
+    Serial.println("No Fire");
+    digitalWrite(51, LOW);
+    break;
+  }
+  delay(50);  // delay between reads
+
+//MOTOR DRIVE
+  if(Serial1.available() > 0){ 
+    command = Serial1.read(); 
     Stop(); //initialize with motors stoped
     //Change pin mode only if new command is different from previous.   
     //Serial.println(command);
@@ -36,7 +61,6 @@ void loop(){
     }
   } 
 }
-
 void forward()
 {
   motor1.setSpeed(255); //Define maximum velocity
